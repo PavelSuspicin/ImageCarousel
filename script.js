15122c8abc3771a1b91c6898318a63928df26b7d
Pavel <pavelsuspicin@gmail.com>
2022-05-13 12:17:27 +0300

    13.05.22

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+const imgs = document.getElementById('imgs')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+const img = document.querySelectorAll('#imgs img')
 
+let ind = 0
+let interval = setInterval(run, 2000)
+
+function run() {
+    ind++
+    changeImage()
+}
+
+function changeImage() {
+    if (ind > img.length - 1) {
+        ind = 0
+    } else if (ind < 0) {
+        ind = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-ind * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
+
+rightBtn.addEventListener('click', () => {
+    ind++
+    changeImage()
+    resetInterval()
+})
+
+leftBtn.addEventListener('click', () => {
+    ind--
+    changeImage()
+    resetInterval()
+})
